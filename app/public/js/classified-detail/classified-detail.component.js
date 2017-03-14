f578415e9593b6213a2e621d104c67e2b5113f2c
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2017-03-14 16:48:46 -0600

    it all works all routes

--- app/public/js/classified-detail/classified-detail.component.js
+++ app/public/js/classified-detail/classified-detail.component.js
@@ -6,31 +6,42 @@
     templateUrl: '/js/classified-detail/classified-detail.template.html',
     controller: controller
   })
-  controller.$inject = ['$http', '$stateParams']
-  function controller ($http, $stateParams) {
+  controller.$inject = ['$http', '$stateParams', '$state']
+  function controller ($http, $stateParams, $state) {
     const vm = this
 
 
     vm.$onInit = onInit
     vm.updateClassified = updateClassified
     // vm.patchClassified = patchClassified
+    vm.deleteClassified = deleteClassified
 
     function onInit() {
       vm.id = $stateParams.id
       $http.get(`/classifieds/${$stateParams.id}`)
       .then(response => {
-        console.log(response.data);
+        // console.log(response.data);
         vm.classifiedResponse = response.data;
 
       })
 
     }
-  }
-  function updateClassified(){
-    $http.patch(`/classifieds/${stateParams.id}`,vm.classified)
-    .then(response => {
-      $state.go('/:id')
-    })
+    function updateClassified(){
+      console.log("Hello");
+      $http.patch(`/classifieds/${$stateParams.id}`,vm.classifiedResponse)
+      .then(response => {
+        // console.log("response is",response);
+        $state.go('home')
+      })
+    }
+    function deleteClassified(){
+      console.log("Hello");
+      $http.delete(`/classifieds/${$stateParams.id}`)
+      .then(response => {
+        console.log("response is",response);
+        $state.go('home')
+      })
+    }
   }
 
 }())
