cd537fa4150008d0ed42d0f48566c70e9c7c7231
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2017-03-14 11:53:49 -0600

    Logs the classified now

--- app/public/js/app/app.component.js
+++ app/public/js/app/app.component.js
@@ -13,11 +13,19 @@
     const vm = this;
 
     vm.$onInit = onInit;
+    vm.createClassified = createClassified;
 
     function onInit() {
       $http.get('/classifieds').then(function (response) {
         vm.classifieds = response.data;
       });
+    }
+
+    function createClassified() {
+      // $http.post('/classifieds', vm.classified).then(function (response) {
+      //   vm.classifieds.push(response.data);
+      // });
+      console.log(vm.classified);
     }
   }
 })();
